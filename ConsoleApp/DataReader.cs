bcfe96c398664fad53b417935863972ef0648f50
Slepotek <slepotek@protonmail.com>
2022-08-07 16:01:47 +0200

    commit

--- ConsoleApp/DataReader.cs
+++ ConsoleApp/DataReader.cs
@@ -1,11 +1,14 @@
-﻿namespace ConsoleApp
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+//All things considered the operations included in this algorithm could work on a proper SQL database.
+//Using LINQ expresions and EF would be a lot better idea, however I can think of many situations, where the CSV format is the only way. 
+namespace ConsoleApp
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Text;
-    using System.IO;
-    using System.Linq;
-    using System.Threading.Tasks;
 
     public class DataReader
     {
@@ -13,79 +16,98 @@
 
         public void ImportAndPrintData(string fileToImport, bool printData = true)
         {
+            //A type created for storing the table (TextFieldParser or CsvHelper would be probably a better idea here)
             ImportedObjects = new List<ImportedObject>() { new ImportedObject() };
-
-            var streamReader = new StreamReader(fileToImport);
-
-            var importedLines = new List<string>();
-            while (!streamReader.EndOfStream)
+            //Opening a stream to file
+            using(var streamReader = new StreamReader(fileToImport))//if not .NET tool then resource management should be in place
             {
-                var line = streamReader.ReadLine();
-                importedLines.Add(line);
-            }
+                //A helper variable to convey the CSV data
+                var importedLines = new List<string>();
+                while (!streamReader.EndOfStream)//until no end of file
+                {
+                    //save one line of CSV file
+                    var line = streamReader.ReadLine();
+                    //And add that line to the list 
+                    importedLines.Add(line);
+                }
 
-            for (int i = 0; i <= importedLines.Count; i++)
-            {
-                var importedLine = importedLines[i];
-                var values = importedLine.Split(';');
-                var importedObject = new ImportedObject();
-                importedObject.Type = values[0];
-                importedObject.Name = values[1];
-                importedObject.Schema = values[2];
-                importedObject.ParentName = values[3];
-                importedObject.ParentType = values[4];
-                importedObject.DataType = values[5];
-                importedObject.IsNullable = values[6];
-                ((List<ImportedObject>)ImportedObjects).Add(importedObject);
+                //deserialize the read file into ImportedObject type
+                for (int i = 0; i < importedLines.Count; i++)
+                {
+                    var importedLine = importedLines[i];
+                    if (importedLine != string.Empty)
+                    {
+                        var values = importedLine.Split(';');
+                        var importedObject = new ImportedObject();
+                        importedObject.Type = values[0];
+                        importedObject.Name = values[1];
+                        importedObject.schema = values[2];
+                        importedObject.parentName = values[3];
+                        importedObject.ParentType = values[4];
+                        importedObject.DataType = values[5];
+                        importedObject.IsNullable = values[6];
+                        ((List<ImportedObject>)ImportedObjects).Add(importedObject);
+                    }
+
+                }
             }
 
             // clear and correct imported data
+            // improved the null exception handling by adding the null conditional
             foreach (var importedObject in ImportedObjects)
             {
-                importedObject.Type = importedObject.Type.Trim().Replace(" ", "").Replace(Environment.NewLine, "").ToUpper();
-                importedObject.Name = importedObject.Name.Trim().Replace(" ", "").Replace(Environment.NewLine, "");
-                importedObject.Schema = importedObject.Schema.Trim().Replace(" ", "").Replace(Environment.NewLine, "");
-                importedObject.ParentName = importedObject.ParentName.Trim().Replace(" ", "").Replace(Environment.NewLine, "");
-                importedObject.ParentType = importedObject.ParentType.Trim().Replace(" ", "").Replace(Environment.NewLine, "");
+                importedObject.Type = importedObject.Type?.Trim().Replace(" ", "").Replace(Environment.NewLine, "").ToUpper();
+                importedObject.Name = importedObject.Name?.Trim().Replace(" ", "").Replace(Environment.NewLine, "");
+                importedObject.schema = importedObject.schema?.Trim().Replace(" ", "").Replace(Environment.NewLine, "");
+                importedObject.parentName = importedObject.parentName?.Trim().Replace(" ", "").Replace(Environment.NewLine, "");
+                importedObject.ParentType = importedObject.ParentType?.Trim().Replace(" ", "").Replace(Environment.NewLine, "");
             }
 
             // assign number of children
-            for (int i = 0; i < ImportedObjects.Count(); i++)
+            // incressed the start index to skip the header line and null line
+            for (int i = 2; i < ImportedObjects.Count(); i++)
             {
-                var importedObject = ImportedObjects.ToArray()[i];
-                foreach (var impObj in ImportedObjects)
+                var importedObject = ImportedObjects.ToArray()[i];//assign the first element of ImpoprtedObjects to local variable
+
+                foreach (var impObj in ImportedObjects) //loop through elements of ImportedObjects
                 {
+                    //match the Parent.Type and Parent.Name
                     if (impObj.ParentType == importedObject.Type)
                     {
-                        if (impObj.ParentName == importedObject.Name)
+                        if (impObj.parentName == importedObject.Name)
                         {
-                            importedObject.NumberOfChildren = 1 + importedObject.NumberOfChildren;
+                            //If both criteria are filled than increse the number of children
+                            importedObject.numberOfChildren = 1 + importedObject.numberOfChildren;
+                            //the importedObject variable should be saved somewhere?? otherwise it is useless
+                            //added the assignment of the updated value to proper filed in the database object
+                            ((List<ImportedObject>)ImportedObjects)[i].numberOfChildren = importedObject.numberOfChildren;
                         }
                     }
                 }
             }
-
+            //loop through database object and print only "DATABASE" type
             foreach (var database in ImportedObjects)
             {
                 if (database.Type == "DATABASE")
                 {
-                    Console.WriteLine($"Database '{database.Name}' ({database.NumberOfChildren} tables)");
+                    Console.WriteLine($"Database '{database.Name}' ({database.numberOfChildren} tables)");
 
                     // print all database's tables
                     foreach (var table in ImportedObjects)
                     {
-                        if (table.ParentType.ToUpper() == database.Type)
+                        //print only children
+                        if (table.ParentType?.ToUpper() == database?.Type)
                         {
-                            if (table.ParentName == database.Name)
+                            if (table.parentName == database.Name)
                             {
-                                Console.WriteLine($"\tTable '{table.Schema}.{table.Name}' ({table.NumberOfChildren} columns)");
+                                Console.WriteLine($"\tTable '{table.schema}.{table.Name}' ({table.numberOfChildren} columns)");
 
                                 // print all table's columns
                                 foreach (var column in ImportedObjects)
                                 {
-                                    if (column.ParentType.ToUpper() == table.Type)
+                                    if (column.ParentType?.ToUpper() == table?.Type)
                                     {
-                                        if (column.ParentName == table.Name)
+                                        if (column.parentName == table.Name)
                                         {
                                             Console.WriteLine($"\t\tColumn '{column.Name}' with {column.DataType} data type {(column.IsNullable == "1" ? "accepts nulls" : "with no nulls")}");
                                         }
@@ -103,14 +125,14 @@
 
     class ImportedObject : ImportedObjectBaseClass
     {
-        public string Name
+        public string Name //Why? I would delete this and use the base class but it could be needed for some kind of naming convention or compatibility with other object types
         {
             get;
             set;
         }
-        public string Schema;
+        public string schema; //if those are variables and not properities they should be written with lowercase firs letter
 
-        public string ParentName;
+        public string parentName;//although they probably should all be properties
         public string ParentType
         {
             get; set;
@@ -119,7 +141,7 @@
         public string DataType { get; set; }
         public string IsNullable { get; set; }
 
-        public double NumberOfChildren;
+        public double numberOfChildren;
     }
 
     class ImportedObjectBaseClass
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,18 +1,19 @@
-﻿namespace ConsoleApp
-{
-    using System;
-    using System.Collections.Generic;
-    using System.IO;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
+﻿//mostly useless here but left for possible future changes and reference
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace ConsoleApp
+{
     internal class Program
     {
         static void Main(string[] args)
         {
             var reader = new DataReader();
-            reader.ImportAndPrintData("dataa.csv");
+            reader.ImportAndPrintData("data.csv");
         }
     }
 }
